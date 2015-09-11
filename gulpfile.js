5febfdb43a9b75071ab7e98de749552c9ce92421
Christopher Varakliotis <c.varakliotis@gmail.com>
2015-09-11 13:53:35 +0300

    Updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,24 @@
 // Load plugins
 var gulp = require('gulp');
+
 var notifier = require('node-notifier');
 var util = require('gulp-util');
+
+var LessPluginCleanCss = require('less-plugin-clean-css'),
+	cleanCss = new LessPluginCleanCss({
+		keepSpecialComments: 0
+	});
+
 var webpack = require('gulp-webpack');
 var webpackConfig = require('./webpack.config');
 var connect = require('gulp-connect');
 var preprocess = require('gulp-preprocess');
 var less = require('gulp-less');
-var minifycss = require('gulp-minify-css');
+var inlinesource = require('gulp-inline-source');
 var jshint = require('gulp-jshint');
 var uglify = require('gulp-uglify');
 var imagemin = require('gulp-imagemin');
 var rename = require('gulp-rename');
-var cache = require('gulp-cache');
 var livereload = require('gulp-livereload');
 var del = require('del');
 var runSequence = require('run-sequence');
@@ -33,6 +39,14 @@ function errorHandler(err) {
 	this.emit('end');
 }
 
+// Error handler
+function lessVerboseErrorHandler(err) {
+	// Log to console
+	util.log(util.colors.red('Error'), err.message);
+	// Manually end the stream, so that it can re-run
+	this.emit('end');
+}
+
 // Connect dev server
 gulp.task('connectDev', function() {
 	connect.server({
@@ -47,7 +61,7 @@ gulp.task('connectDev', function() {
 gulp.task('connect', function() {
 	connect.server({
 		root: buildPath,
-		port: 8888,
+		port: 8080,
 		hostname: '*', // to allow access to server from outside
 		livereload: true
 	});
@@ -55,7 +69,14 @@ gulp.task('connect', function() {
 
 // Copy HTML
 gulp.task('copyHtml', function() {
-	return gulp.src('./index.html').pipe(gulp.dest(buildPath));
+	return gulp.src('./index.html')
+		.pipe(preprocess({
+			context: {
+				NODE_ENV: 'development',
+				DEV: true
+			}
+		}))
+		.pipe(gulp.dest(buildPath));
 });
 
 // Preprocess HTML
@@ -63,26 +84,26 @@ gulp.task('preprocessHtml', function() {
 	return gulp.src('./index.html')
 		.pipe(preprocess({
 			context: {
-				NODE_ENV: 'PRODUCTION',
-				DEBUG: true
+				NODE_ENV: 'production',
+				PROD: true
 			}
 		}))
-		.pipe(rename({
-			basename: 'index'
-		}))
+		.pipe(inlinesource())
 		.pipe(gulp.dest(buildPath));
 });
 
 // Styles
 gulp.task('styles', function() {
 	return gulp.src('./assets/css/style.less')
-		.pipe(less())
+		// Run the transformation from LESS to CSS & minify
+		.pipe(less({
+			plugins: [cleanCss]
+		}))
 		.on('error', errorHandler)
 		.pipe(rename({
 			basename: 'main',
 			suffix: '.min',
 		}))
-		.pipe(minifycss())
 		.pipe(gulp.dest(buildPath + '/assets/css'))
 		.pipe(connect.reload());
 });
@@ -107,19 +128,24 @@ gulp.task('webpack', function() {
 gulp.task('uglifyBundle', function() {
 	return gulp.src(buildPath + '/assets/js/*.js')
 		.pipe(uglify())
-		.pipe(gulp.dest(buildPath + '/assets/js'))
-		.pipe(connect.reload());
+		.pipe(gulp.dest(buildPath + '/assets/js'));
 });
 
 // Images
 gulp.task('images', function() {
-	return gulp.src('./assets/img/**/*')
-		.pipe(cache(imagemin({
+	return gulp.src('./assets/img/**/*.*')
+		.pipe(imagemin({
 			optimizationLevel: 3,
 			progressive: true,
 			interlaced: true
-		})))
-		.pipe(gulp.dest(buildPath + '/assets/img'));
+		}))
+		.pipe(gulp.dest(buildPath + '/assets/img/'));
+});
+
+// Favicons
+gulp.task('favicons', function() {
+	return gulp.src('./assets/favicons/*.*')
+		.pipe(gulp.dest(buildPath + '/assets/favicons'));
 });
 
 // Fonts
@@ -134,14 +160,16 @@ gulp.task('clean', function(cb) {
 	del([buildPath], cb);
 });
 
-// Build
+// Build (for dev)
 gulp.task('build', function(callback) {
-	runSequence('clean', 'webpack', ['styles', 'fonts', 'images', 'copyHtml'], callback);
+	var start = new Date().getTime();
+	runSequence('clean', 'webpack', 'styles', ['fonts', 'favicons', 'images'], 'copyHtml', callback);
 });
 
-// Build Plus
+// Build Plus (for prod - takes more time)
 gulp.task('buildPlus', function(callback) {
-	runSequence('build', 'uglifyBundle', callback);
+	var start = new Date().getTime();
+	runSequence('clean', 'webpack', 'styles', ['fonts', 'favicons', 'images'], 'preprocessHtml', 'uglifyBundle', callback);
 });
 
 // Watch
