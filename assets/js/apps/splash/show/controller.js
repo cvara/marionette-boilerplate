19c196280907488c11293e3bcca6dd8817bac973
Christopher Varakliotis <c.varakliotis@gmail.com>
2016-10-18 14:58:06 +0300

    Splash shown in main region

--- assets/js/apps/splash/show/controller.js
+++ assets/js/apps/splash/show/controller.js
@@ -6,7 +6,7 @@ const Controller = {};
 
 Controller.showSplash = () => {
 	const splashView = new View.Splash();
-	App.rootView.showChildView('overlay', splashView);
+	App.rootView.showChildView('main', splashView);
 };
 
 export default Controller;
